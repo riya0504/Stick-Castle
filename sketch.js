4584ee4585f9bdc7f91cbbfe720178960ddd0cbb
riya0504 <63499475+riya0504@users.noreply.github.com>
2020-06-07 20:17:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup(){
+    var canvas = createCanvas(800,400);
+
+}
+
+function draw(){
+    background(0);
+    fill('#fae');
+    rect(400,200, 100, 300);
+    rect(330,250, 70, 160);
+    rect(500,250, 70, 160);
+    triangle(450,100,400,200,500,200);
+    square(264,340,65)
+    square(571,340,65)
+    rect(221,200,50, 200);
+    rect(629,200,50, 200);
+
+}
